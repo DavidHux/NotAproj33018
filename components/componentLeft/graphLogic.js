a4827c653977dacdfdc0758f0e180dc639c9f58c
David Hu <hux0713@gmail.com>
2017-07-05 13:05:45 +0800

    improve logic graph

--- components/componentLeft/graphLogic.js
+++ components/componentLeft/graphLogic.js
@@ -5,11 +5,137 @@ import echarts from 'echarts'
 import serviceStore from '../../stores/serviceStore'
 import ServiceActions from '../../actions/ServiceActions'
 var fdata = require('../../data/mydata')
-// var ecConfig = echarts.config.EVENT
 
-// const GraphLogic = () => (
+var addindex = 0
+var data1 = [
+    [{
+            "id": "广东水文",
+            "group": 3,
+            "color": "#d48265"
+        },
+        {
+            "id": "福建水文",
+            "group": 3,
+            "color": "#d48265"
+        }
+    ],
 
-// )
+    [{
+            "id": "江西水文",
+            "group": 3,
+            "color": "#d48265"
+        },
+        {
+            "id": "福建水文",
+            "group": 3,
+            "color": "#d48265"
+        }
+
+    ],
+    [{
+            "id": "浙江水文",
+            "group": 3,
+            "color": "#d48265"
+        },
+        {
+            "id": "江西水文",
+            "group": 3,
+            "color": "#d48265"
+        },
+        {
+            "id": "安徽水文",
+            "group": 3,
+            "color": "#d48265"
+        }
+    ],
+    [{
+            "id": "浙江水文",
+            "group": 3,
+            "color": "#d48265"
+        },
+        {
+            "id": "安徽水文",
+            "group": 3,
+            "color": "#d48265"
+        },
+        {
+            "id": "江苏水文",
+            "group": 3,
+            "color": "#d48265"
+        },
+        {
+            "id": "上海水文",
+            "group": 3,
+            "color": "#d48265"
+        }
+    ],
+    []
+]
+var timeo = [95000, 160000, 180000, 195000, 220000]
+var data2 = [
+    [{
+            "source": "水文",
+            "target": "广东水文",
+            "value": 6
+        },
+        {
+            "source": "水文",
+            "target": "福建水文",
+            "value": 6
+        }
+    ],
+    [{
+            "source": "水文",
+            "target": "江西水文",
+            "value": 6
+        },
+        {
+            "source": "水文",
+            "target": "福建水文",
+            "value": 6
+        }
+    ],
+    [{
+            "source": "水文",
+            "target": "浙江水文",
+            "value": 6
+        },
+        {
+            "source": "水文",
+            "target": "安徽水文",
+            "value": 6
+        },
+        {
+            "source": "水文",
+            "target": "江西水文",
+            "value": 6
+        },
+    ],
+    [{
+            "source": "水文",
+            "target": "浙江水文",
+            "value": 6
+        },
+        {
+            "source": "水文",
+            "target": "安徽水文",
+            "value": 6
+        },
+
+        {
+            "source": "水文",
+            "target": "江苏水文",
+            "value": 6
+        },
+        {
+            "source": "水文",
+            "target": "上海水文",
+            "value": 6
+        }
+    ],
+    []
+]
+var myChart = null
 
 export default class GraphLogic extends React.Component {
     constructor(props) {
@@ -17,10 +143,36 @@ export default class GraphLogic extends React.Component {
     }
     eConsole(param) {
         console.log(param)
+        if(param.dataType == 'node'){
+            window.location.href = '/#/logicView/'+param.name
+        }
+    }
+
+    modNode(node) {
+        return {
+            x: null,
+            y: null,
+            draggable: true,
+            id: node.id,
+            name: node.id,
+            symbolSize: 10,
+            itemStyle: {
+                normal: {
+                    color: node.color
+                }
+            }
+        };
+    }
+    modLink(edge) {
+        return {
+            source: edge.source,
+            target: edge.target
+        };
     }
 
     componentDidMount() {
-        var myChart = echarts.init(document.getElementById('myChart0'));
+        var that = this
+        myChart = echarts.init(document.getElementById('myChart0'));
         //npm dependences graph http://echarts.baidu.com/demo.html#graph-npm
         myChart.showLoading();
         $.getJSON('/data/asset/data/npmdepgraph.min10.json', function (json1) {
@@ -29,44 +181,29 @@ export default class GraphLogic extends React.Component {
             myChart.hideLoading();
             myChart.setOption({
                 title: {
-                    text: 'NPM Dependencies'
+                    text: ''
                 },
                 animationDurationUpdate: 1500,
                 animationEasingUpdate: 'quinticInOut',
+                tooltip: {},
+
                 series: [{
                     type: 'graph',
                     layout: 'force',
                     force: {
-                    repulsion: 100
+                        repulsion: 100
                     },
                     // progressiveThreshold: 700,
-                    data: json.nodes.map(function (node) {
-                        return {
-                            x: null,
-                            y: null,
-                            draggable: true,
-                            id: node.id,
-                            name: node.id,
-                            symbolSize: 10,
-                            itemStyle: {
-                                normal: {
-                                    color: node.color
-                                }
-                            }
-                        }; 
-                    }),
-                    links: json.links.map(function (edge) {
-                        return {
-                            source: edge.source,
-                            target: edge.target
-                        };
-                    }),
+                    data: json.nodes.map(that.modNode),
+                    links: json.links.map(that.modLink),
                     label: {
-                        show: true,
-                        emphasis: {
-                            position: 'right',
-                            show: true
+                        normal: {
+                            show: true,
+                            position: 'right'
                         }
+                        // emphasis: {
+                        //     show: true
+                        // }
                     },
                     roam: true,
                     focusNodeAdjacency: true
@@ -80,43 +217,96 @@ export default class GraphLogic extends React.Component {
                 }]
             }, true);
         });
-        myChart.on('click', this.eConsole)  
+        myChart.on('click', this.eConsole)
+            setTimeout(() => {
+                var i = 0
+                console.log(addindex)
+                console.log(data1[i])
+                var dataTnodes = fdata.nodes.concat(data1[i])
+                var dataTlinks = fdata.links.concat(data2[i])
+                console.log(dataTlinks, dataTnodes)
+                myChart.setOption({
+                    series: [{
+                        data: dataTnodes.map(that.modNode),
+                        links: dataTlinks.map(that.modLink)
+                    }]
+                });
+
+            }, timeo[0])
+            setTimeout(() => {
+                var i = 1
+                var dataTnodes = fdata.nodes.concat(data1[i])
+                var dataTlinks = fdata.links.concat(data2[i])
+                console.log(dataTlinks, dataTnodes)
+                myChart.setOption({
+                    series: [{
+                        data: dataTnodes.map(that.modNode),
+                        links: dataTlinks.map(that.modLink)
+                    }]
+                });
+
+            }, timeo[1])
+            setTimeout(() => {
+                var i = 2
+                var dataTnodes = fdata.nodes.concat(data1[i])
+                var dataTlinks = fdata.links.concat(data2[i])
+                console.log(dataTlinks, dataTnodes)
+                myChart.setOption({
+                    series: [{
+                        data: dataTnodes.map(that.modNode),
+                        links: dataTlinks.map(that.modLink)
+                    }]
+                });
+
+            }, timeo[2])
+            setTimeout(() => {
+                var i = 3
+                var dataTnodes = fdata.nodes.concat(data1[i])
+                var dataTlinks = fdata.links.concat(data2[i])
+                console.log(dataTlinks, dataTnodes)
+                myChart.setOption({
+                    series: [{
+                        data: dataTnodes.map(that.modNode),
+                        links: dataTlinks.map(that.modLink)
+                    }]
+                });
+
+            }, timeo[3])
+            setTimeout(() => {
+                var i = 4
+                var dataTnodes = fdata.nodes.concat(data1[i])
+                var dataTlinks = fdata.links.concat(data2[i])
+                console.log(dataTlinks, dataTnodes)
+                myChart.setOption({
+                    series: [{
+                        data: dataTnodes.map(that.modNode),
+                        links: dataTlinks.map(that.modLink)
+                    }]
+                });
+
+            }, timeo[4])
+        // setInterval(function () {
+
+        //     if(addindex >= data1.length)
+        //         return
+        //     fdata.nodes.push(data1[addindex])
+        //     fdata.links.push(data2[addindex])
+        //     addindex++
+
+        //     myChart.setOption({
+        //         series: [{
+        //             data: fdata.nodes.map(that.modNode),
+        //             links: fdata.links.map(that.modLink)
+        //         }]
+        //     });
+        // }, 5000);
     }
 
     render() {
         return (
-            <div>
+             <div>
                 <div id="myChart0" style={{ "width": "100%", "height": "400px"}}></div>
             </div>
         )
     }
 }
-
-// option = {
-//         legend: [{
-//             // selectedMode: 'single',
-//             data: categories.map(function (a) {
-//                 return a.name;
-//             })
-//         }],
-//         animation: false,
-//         series : [
-//             {
-//                 name: 'Les Miserables',
-//                 type: 'graph',
-//                 layout: 'force',
-//                 data: graph.nodes,
-//                 links: graph.links,
-//                 categories: categories,
-//                 roam: true,
-//                 label: {
-//                     normal: {
-//                         position: 'right'
-//                     }
-//                 },
-//                 force: {
-//                     repulsion: 100
-//                 }
-//             }
-//         ]
-//     };
