b14d7b9733362b1a3bdfa2e128b39c22352c64b1
David Hu <hux0713@gmail.com>
2017-07-11 18:47:32 +0800

    fix listStore missing problem

--- dispatcher/AppDispatcher.js
+++ dispatcher/AppDispatcher.js
@@ -1,14 +1,14 @@
 var Dispatcher = require('flux').Dispatcher;
 var AppDispatcher = new Dispatcher();
-var ListStore = require('../stores/ListStore');
+// var ListStore = require('../stores/ListStore');
 import serviceStore from '../stores/serviceStore'
 
 AppDispatcher.register(function (action) {
   switch(action.actionType) {
-    case 'ADD_NEW_ITEM':
-      ListStore.addNewItemHandler(action.text);
-      ListStore.emitChange();
-      break;
+    // case 'ADD_NEW_ITEM':
+    //   ListStore.addNewItemHandler(action.text);
+    //   ListStore.emitChange();
+    //   break;
     case 'UPDATE_SERVICE_LIST':
       serviceStore.updateServiceListHandler(action.o)
       serviceStore.emitServiceChange()
