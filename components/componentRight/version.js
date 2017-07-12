c56115d1fa27d608e3966df527ebacc414cbc02a
David Hu <hux0713@gmail.com>
2017-07-12 13:20:11 +0800

    fix some bugs

--- components/componentRight/version.js
+++ components/componentRight/version.js
@@ -20,10 +20,10 @@ var list = [
     ["暴雨预警", "雷电预警", "暴雪预警"]
 ]
 
-function getarrindex(aaa){
-    for(var i = 0;i < 3;i++){
-        for(var j = 0;j< list[i].length;j++){
-            if(list[i][j] == aaa){
+function getarrindex(aaa) {
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < list[i].length; j++) {
+            if (list[i][j] == aaa) {
                 return i
             }
         }
@@ -74,13 +74,13 @@ export default class version extends React.Component {
         global.window.onmousemove = this.movetooltip.bind(this)
 
     }
-    componentWillUpdate(){
+    componentWillUpdate() {
         // if(this.state.previousID == null) return
         // var element = document.getElementById("gitGraph"+ this.state.previousID);
         // console.log('remove element', element)
         // if(element != null) element.parentNode.removeChild(element)
     }
-    getCurrentVersion(callback){
+    getCurrentVersion(callback) {
         $.ajax({
             url: '/v2/apps/nap/ityphoon',
             type: 'GET',
@@ -91,43 +91,45 @@ export default class version extends React.Component {
             }
             // beforeSend: setHeader
         });
-        function processData(json){
+
+        function processData(json) {
             var image = json.app.container.docker.image
             var id = image.substring(image.lastIndexOf(":") + 1)
             // console.log('current version :', id)
             callback(id)
         }
     }
-    computeNodeAt(id){
-        for(var i = 0;i < this.state.commits.length;i++){
-            if(this.state.commits[i].short_id == id){
+    computeNodeAt(id) {
+        for (var i = 0; i < this.state.commits.length; i++) {
+            if (this.state.commits[i].short_id == id) {
                 return this.state.commits.length - 1 - i
             }
         }
         return -1
     }
     componentDidUpdate() {
-        if (this.props.ID == 'version') return;
-        
-        // console.log('update')
+        var IDlast = window.location.href.substring(window.location.href.lastIndexOf('/') + 1)
+        if (IDlast == 'version') return;
+
+        // console.log('update', this.props.ID)
         var that = this
         var num = getarrindex(this.props.ID)
         this.getCurrentVersion(processGraph)
 
-        function processGraph(id){
+        function processGraph(id) {
             // console.log('process graph: id', id)
-        // if (this.state.num > 0) {
+            // if (this.state.num > 0) {
             // if (num == 0) {
-                $.ajax({
-                    url: '/api/v4/projects/431/repository/commits',
-                    type: 'GET',
-                    dataType: 'json',
-                    success: processJSON,
-                    error: function (e) {
-                        console.log(e), alert('boo!');
-                    },
-                    beforeSend: setHeader
-                });
+            $.ajax({
+                url: '/api/v4/projects/431/repository/commits',
+                type: 'GET',
+                dataType: 'json',
+                success: processJSON,
+                error: function (e) {
+                    console.log(e), alert('boo!');
+                },
+                beforeSend: setHeader
+            });
             // } else {
             //     var datas = data[num - 1]
             //     processJSON(datas)
@@ -143,7 +145,7 @@ export default class version extends React.Component {
                 if (json == null)
                     return
                 var nodeindex = that.computeNodeAt(id)
-                if(nodeindex == -1){
+                if (nodeindex == -1) {
                     console.log('compute node index -1', that.state.commits, id)
                 }
                 that.state.nodeAt = nodeindex
@@ -152,7 +154,7 @@ export default class version extends React.Component {
                     template: myTemplate, // or blackarrow
                     orientation: "vertical-reverse",
                     author: "John Doe",
-                    elementId : "gitGraph" + that.props.ID,
+                    elementId: "gitGraph" + that.props.ID,
                     mode: "extended" // or compact if you don't want the messages
                 })
                 var branches = []
@@ -162,48 +164,54 @@ export default class version extends React.Component {
                         that.commit(master, json[i], json.length - 1 - i)
                     }
                 }
-                       
-                that.state.gitGraph.canvas.addEventListener("commit:mouseover", function(e){this.style.cursor = "pointer";that.showtooltip.bind(that)(e)})
-                that.state.gitGraph.canvas.addEventListener("commit:mouseout", function(e){this.style.cursor = "auto";that.hidetooltip.bind(that)(e)})
+
+                that.state.gitGraph.canvas.addEventListener("commit:mouseover", function (e) {
+                    this.style.cursor = "pointer";
+                    that.showtooltip.bind(that)(e)
+                })
+                that.state.gitGraph.canvas.addEventListener("commit:mouseout", function (e) {
+                    this.style.cursor = "auto";
+                    that.hidetooltip.bind(that)(e)
+                })
                 // that.state.gitGraph.canvas.addEventListener("commit:mousemove", that.movetooltip.bind(that))                
             }
         }
     }
-    computeIndex(hash){
-        for(var i = 0;i < this.state.commits.length;i++){
-            if(this.state.commits[i].short_id == hash){
-                return this.state.commits.length - 1 -i
+    computeIndex(hash) {
+        for (var i = 0; i < this.state.commits.length; i++) {
+            if (this.state.commits[i].short_id == hash) {
+                return this.state.commits.length - 1 - i
             }
         }
         return -1
     }
-    changeVersion(versionID){
+    changeVersion(versionID) {
         $.ajax({
-                    url: 'https://git.njuics.cn/api/v4/projects/431/trigger/pipeline',
-                    type: 'POST',
-                    data: {
-                        token: "b6554b434c272d3077952ccdd2a134",
-                        ref: "master",
-                        "variables[SKIP_BUILD]": "true",
-                        "variables[CI_COMMIT_SHA]": versionID
-                    },
-                    // dataType: 'json',
-                    // success: processJSON,
-                    error: function (e) {
-                        console.log("change version failed", e)
-                    }
-                    // beforeSend: setHeader
-                }).done(function( msg ) {
-                    console.log('change version success:', msg)
-                });
+            url: 'https://git.njuics.cn/api/v4/projects/431/trigger/pipeline',
+            type: 'POST',
+            data: {
+                token: "b6554b434c272d3077952ccdd2a134",
+                ref: "master",
+                "variables[SKIP_BUILD]": "true",
+                "variables[CI_COMMIT_SHA]": versionID
+            },
+            // dataType: 'json',
+            // success: processJSON,
+            error: function (e) {
+                console.log("change version failed", e)
+            }
+            // beforeSend: setHeader
+        }).done(function (msg) {
+            console.log('change version success:', msg)
+        });
     }
 
     onNodeClick(param) {
         var i = this.computeIndex(param.sha1)
-        if(i == -1) return
-        if(i == this.state.nodeAt) return
-        if(i == this.state.deployingNode) return
-        if(this.state.deployingNode == true) return
+        if (i == -1) return
+        if (i == this.state.nodeAt) return
+        if (i == this.state.deployingNode) return
+        if (this.state.deploying == true) return
 
         var that = this
         var yp = -param.y / 50
@@ -212,10 +220,13 @@ export default class version extends React.Component {
         this.changeVersion(id)
         // this.changeVersion("7c5219e4")
         this.state.deployingNode = yp
-        this.setState({ deploying: true})
+        this.setState({
+            deploying: true
+        })
         // setTimeout(()=>{this.setState({ deploying : false, nodeAt : yp})}, 3000)
         setTimeout(getResponse, 8000)
-        function getResponse(){
+
+        function getResponse() {
             $.ajax({
                 type: 'GET',
                 url: '/v2/apps/nap/ityphoon',
@@ -224,15 +235,19 @@ export default class version extends React.Component {
                 error: function (e) {
                     console.log('get current version failed', e)
                 }
-            // beforeSend: setHeader
+                // beforeSend: setHeader
             });
-            function processData1(json){
+
+            function processData1(json) {
                 var image = json.app.container.docker.image
                 var id = image.substring(image.lastIndexOf(":") + 1)
                 // console.log("proccess callback", that.state.commits[that.state.commits.length - 1 - that.state.deployingNode].short_id, id)
-                if(that.state.commits[that.state.commits.length - 1 - that.state.deployingNode].short_id == id){
-                    if(json.app.deployments.length == 0){
-                        that.setState({deploying: false, nodeAt: that.state.deployingNode})
+                if (that.state.commits[that.state.commits.length - 1 - that.state.deployingNode].short_id == id) {
+                    if (json.app.deployments.length == 0) {
+                        that.setState({
+                            deploying: false,
+                            nodeAt: that.state.deployingNode
+                        })
                     } else {
                         setTimeout(getResponse, 3000)
                     }
@@ -241,40 +256,50 @@ export default class version extends React.Component {
                 }
             }
         }
-        setTimeout(() => {depNodeColor("#979797", "#fac21b")}, 500)
-        function depNodeColor(color1, color2){
-            that.setState({deployNodeColor: color1})
-            if(that.state.deploying == true){
-            // console.log("dep node color", color1, color2)
-                setTimeout(() => {depNodeColor(color2, color1)}, 500)
+        setTimeout(() => {
+            depNodeColor("#979797", "#fac21b")
+        }, 500)
+
+        function depNodeColor(color1, color2) {
+            that.setState({
+                deployNodeColor: color1
+            })
+            if (that.state.deploying == true) {
+                // console.log("dep node color", color1, color2)
+                setTimeout(() => {
+                    depNodeColor(color2, color1)
+                }, 500)
             }
         }
     }
-    showtooltip(e){
+    componentWillUnmount() {
+        this.state.deploying = false
+    }
+    showtooltip(e) {
         var i = this.computeIndex(e.data.sha1)
-        if(i == -1) return
-        if(i == this.state.nodeAt) return
-        if(this.state.btnon == false){
+        if (i == -1) return
+        if (i == this.state.nodeAt) return
+        if (this.state.btnon == false) {
             // console.log('show tooltips ', e)
             this.state.btnon = true
             $(".bs-example-tooltip .tooltip").css("display", "block")
-            if(i < this.state.nodeAt){
+            if (i < this.state.nodeAt) {
                 $(".tooltip-inner").text("click to roll back")
             } else {
                 $(".tooltip-inner").text("click to update")
             }
         }
     }
-    hidetooltip(e){
-        if(this.state.btnon == true){
+    hidetooltip(e) {
+        if (this.state.btnon == true) {
             this.state.btnon = false
             // console.log('hide tooltips', e)
             $(".bs-example-tooltip .tooltip").css("display", "none")
             // global.window.style.cursor = "auto";
         }
     }
-    movetooltip(e){
-        if(this.state.btnon == false) return
+    movetooltip(e) {
+        if (this.state.btnon == false) return
         var x = e.clientX,
             y = e.clientY;
         $(".bs-example-tooltip .tooltip").css({
@@ -287,20 +312,21 @@ export default class version extends React.Component {
     componentDidMount() {
         // if (this.props.ID == 'version') return;   
         // console.log('mount')
+        this.componentDidUpdate()
     }
     commit(branch, o, i) {
         var that = this
         var tag = '',
             color = "white",
             clickFunc = () => {}
-        if (i == that.state.deployingNode && that.state.deploying == true){
+        if (i == that.state.deployingNode && that.state.deploying == true) {
             color = that.state.deployNodeColor
             tag = "Deploy"
         } else if (i == that.state.nodeAt) {
             color = "#34a853"
             tag = "Running"
         } else {
-            if(i < that.state.nodeAt){
+            if (i < that.state.nodeAt) {
                 color = "#979797"
             } else {
                 color = "#008fb5"
@@ -356,42 +382,42 @@ export default class version extends React.Component {
     }
 }
 // } else {
-        //     delete that.state.gitGraph
-        //     that.state.gitGraph = new GitGraph({
-        //         template: myTemplate, // or blackarrow
-        //         orientation: "vertical-reverse",
-        //         author: "John Doe",
-        //         mode: "extended" // or compact if you don't want the messages
-        //     });
-        //     var master = that.state.gitGraph.branch("master");
-        //     that.state.gitGraph.commit("My first commit"); // 1 commit upon HEAD
-        //     var develop = that.state.gitGraph.branch("develop"); // New branch from HEAD
-        //     var myfeature = develop.branch("myfeature"); // New branch from develop
-        //     develop.commit("Develop a feature - part 1");
-        //     develop.commit("Develop a feature - part 2");
+//     delete that.state.gitGraph
+//     that.state.gitGraph = new GitGraph({
+//         template: myTemplate, // or blackarrow
+//         orientation: "vertical-reverse",
+//         author: "John Doe",
+//         mode: "extended" // or compact if you don't want the messages
+//     });
+//     var master = that.state.gitGraph.branch("master");
+//     that.state.gitGraph.commit("My first commit"); // 1 commit upon HEAD
+//     var develop = that.state.gitGraph.branch("develop"); // New branch from HEAD
+//     var myfeature = develop.branch("myfeature"); // New branch from develop
+//     develop.commit("Develop a feature - part 1");
+//     develop.commit("Develop a feature - part 2");
 
-        //     master.commit({
-        //         message: "Fast bugfix",
-        //         author: "John Fixer"
-        //     });
-        //     myfeature.commit({
-        //         message: "New cool feature",
-        //         author: "John Feature"
-        //     });
+//     master.commit({
+//         message: "Fast bugfix",
+//         author: "John Fixer"
+//     });
+//     myfeature.commit({
+//         message: "New cool feature",
+//         author: "John Feature"
+//     });
 
-        //     develop.merge(master);
-        //     myfeature.merge(master);
-        //     master.commit({
-        //         message: "Release of version 0.1",
-        //         tag: "0.1",
-        //         author: "John Releaser",
-        //         sha1: "abcdef0"//,
-        //         // onClick: function (commit) {
-        //         //     console.log(that.state.nodeAt)
-        //         //     that.onNodeClick(commit)
-        //         // }
-        //     });
-        //     // that.state.gitGraph.canvas.addEventListener("commit:mouseover", function (event) {
-        //     //     console.log("You're over a commit.", "Here is a bunch of data ->", event);
-        //     // });
-        // }
+//     develop.merge(master);
+//     myfeature.merge(master);
+//     master.commit({
+//         message: "Release of version 0.1",
+//         tag: "0.1",
+//         author: "John Releaser",
+//         sha1: "abcdef0"//,
+//         // onClick: function (commit) {
+//         //     console.log(that.state.nodeAt)
+//         //     that.onNodeClick(commit)
+//         // }
+//     });
+//     // that.state.gitGraph.canvas.addEventListener("commit:mouseover", function (event) {
+//     //     console.log("You're over a commit.", "Here is a bunch of data ->", event);
+//     // });
+// }
